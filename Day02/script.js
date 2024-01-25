6518f00e440eb20fe90e092fdaba97523c569342
masato-tsuji <v._.vgentarou@ezweb.ne.jp>
2024-01-25 12:02:34 +0900

    edit

--- Day02/script.js
+++ Day02/script.js
@@ -1,6 +1,7 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
+console.log("---------- warm up 1 ----------");
 const variableString = "I am a string";
 console.log(typeof variableString); // "string"と表示されましたか？
 
@@ -15,10 +16,63 @@ console.log(typeof variableBoolean);
 let variableUndefined;
 console.log(typeof variableUndefined);
 
-
+console.log("---------- warm up 2 ----------");
 const numberA = 2
 const numberB = 4
 
 const average = (numberA + numberB) / 2;
 console.log(average); // numberA と numberB の平均が表示される。
+
+console.log("---------- meaningOfLife ----------");
+function meaningOfLife(i) {
+    return 42;
+}
+meaningOfLife();
+
+console.log("---------- addTen ----------");
+function addTen(number) {
+    return number + 10;
+}
+console.log(addTen(5));
+
+console.log("---------- function ----------");
+/*
+addTen {
+    console.log("I am inside!");
+    return number * 10;
+  }
+*/
+  function addTen(number) {
+    console.log("I am inside!");
+    return number + 10;
+  }
+  console.log(addTen(5));
+
+
+  console.log("---------- 4 ----------");
+
+  function subtract(num1, num2) {
+    // ここにコードを書いてください
+    return num1 - num2;
+  }
+
+  console.log(subtract(4, 3)); // => 1
+  console.log(subtract(100, 42)); // => 58
+
+  console.log("---------- 5 ----------");
+// ここにコードを書いてください
+function greeting(name) {
+    return "Hello, " + name + "!";
+}
+
+console.log(greeting("Alex")); // => "Hello, Alex!"
+console.log(greeting("Bob")); // => "Hello, Bob!"
+
+console.log("---------- 6 ----------");
+
+function average2(num1, num2) {
+    return (num1 + num2) / 2;
+}
+
+console.log(average2(2, 3));
 
