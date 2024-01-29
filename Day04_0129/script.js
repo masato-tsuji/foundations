7bdb030edca1a30c67f9fa28e10a78e6fe839e0c
masato-tsuji-office <masato_tsuji@mail.toyota.co.jp>
2024-01-29 12:25:15 +0900

    school

--- Day04_0129/script.js
+++ Day04_0129/script.js
@@ -3,17 +3,93 @@
 
 
 kugiri("worming up1"); //---------------------------------------------
+//isMultipleOfThree という名前の関数を宣言しましょう。引数の数字が 3 の倍数であれば true、
+//そうでなければ false を返してください。
+function isMultipleOfThree(number) {
+  // ここにあなたのコードを書いてください
+  return !(number % 3);
+}
 
+// テスト
+console.log(isMultipleOfThree(6)); // => true
+console.log(isMultipleOfThree(10)); // => false
 
 kugiri("worming up2"); //---------------------------------------------
+//isMultipleOf という名前の関数を宣言しましょう。1 つ目の引数が 2 つ目の引数の倍数であれば true 、
+//そうでなければ false を返してください。
+function isMultipleOf(num1, num2) {
+  // ここにあなたのコードを書いてください
+  return !(num1 % num2);
+}
+
+// テスト
+console.log(isMultipleOf(6, 3)); // => true
+console.log(isMultipleOf(10, 4)); // => false
+console.log(isMultipleOf(-7, 4)); // => false
 
 
+kugiri("kiso4"); //---------------------------------------------
 
-kugiri("kiso1"); //---------------------------------------------
+function biggerNumber(number1, number2) {
+  if (number1 > number2) {
+    return "The first argument is bigger.";
+  } else if (number1 < number2) {
+    return "The second argument is bigger.";
+  }
+}
+
+//テスト
+console.log(biggerNumber(4, 3)); // => 'The first argument is bigger.' と表示されるようにする。
+console.log(biggerNumber(3, 4)); // => 'The second argument is bigger.' と表示されるようにする。
+console.log(biggerNumber(4, 4)); // => 'The second argument is bigger.' と表示されるようにする。
 
 
+kugiri("kiso5"); //---------------------------------------------
 
+function printDataType(data) {
+  if (typeof data === "number") {
+    console.log("This is a number.");
+  } else if (typeof data === "string") {
+    console.log("This is a string.");
+  } else if (typeof data === "boolean") {
+    console.log("This is a boolean.");
+  } else {
+    console.log("This is not a string, boolean, or number.");
+  }
+  // ここにあなたのコードを書いてください
+}
+
+//テストケース
+printDataType(42); // => "This is a number." が表示されるようにする。
+printDataType("Hello!"); // => "This is a string." が表示されるようにする。
+printDataType(true); // => "This is a boolean." が表示されるようにする。
+printDataType(undefined); // => "This is not a string, boolean, or number." が表示されるようにする。
+printDataType("42"); // => "This is a string." が表示されるようにする。
+
+
+kugiri("kiso6"); //---------------------------------------------
+function greeting(name, language) {
+  if (language === "Japanese") {
+    return "Konnichiwa, "  + name + "!";
+  } else if (language === "English") {
+    return "Hello," + " " + name + "!";
+  } else if (language === "German") {
+    return "Gutentag," + " " + name + "!";
+  } else if (language === "Spanish") {
+    return "Hola," + " " + name + "!";
+  }
+}
+
+console.log(greeting("Harry Potter", "Japanese")); // => "Konnichiwa, Harry Potter!" が表示されるようにする。
+console.log(greeting("Harry Potter", "English")); // => "Hello, Harry Potter!" が表示されるようにする。
+console.log(greeting("Harry Potter", "German")); // => "Gutentag, Harry Potter!" が表示されるようにする。
+console.log(greeting("Harry Potter", "Spanish")); // => "Hola, Harry Potter!" が表示されるようにする。
+
+
+/**/
 kugiri("chukyu1"); //---------------------------------------------
+
+
 
 
 
@@ -28,7 +104,16 @@ kugiri("oyo1"); //---------------------------------------------
   黄：&#x1F7E1;<br>
   赤：&#x1F534;
 */
-console.log("\&#x1F534;", "🟢",  Math.floor(Math.random() * 11));
+/**/
+const rnd = Math.floor(Math.random() * 11)
+console.log(rnd);
+if (rnd < 3) {
+  console.log("🔴Red");
+} else if(rnd > 6) {
+  console.log("🟢Green");
+} else {
+  console.log("🟡Yellow");
+}
 
-//switch
+//switchは不等号使えない･･･
 
