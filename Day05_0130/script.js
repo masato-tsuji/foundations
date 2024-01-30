72a66176a8a7418b914d573cd6341fcaed722255
masato-tsuji <v._.vgentarou@ezweb.ne.jp>
2024-01-30 12:44:56 +0900

    edit

--- Day05_0130/script.js
+++ Day05_0130/script.js
@@ -2,7 +2,7 @@
 // 1行目に記載している 'use strict' は削除しないでください
 
 line("worming up1"); //---------------------------------------------
-
+// コードを修正
 // function isHot(temperature) {
 //     if temperature>30 {
 //     return "暑い！";
@@ -16,10 +16,6 @@ function isHot(temperature) {
         return "暑くない";
     }
 }
-  
-console.log(isHot(31)); // "暑い！" を表示
-console.log(isHot(2)); // "暑くない" を表示
-console.log(isHot(30)); // "暑くない" を表示
 
 console.log(isHot(31)); // "暑い！" を表示
 console.log(isHot(2)); // "暑くない" を表示
@@ -27,7 +23,7 @@ console.log(isHot(30)); // "暑くない" を表示
 
 
 line("worming up2"); //---------------------------------------------
-
+// コードを修正
 // function getTypeOfSchool(age) {
 //     if age < 6
 //     return "幼稚園";
@@ -65,8 +61,129 @@ console.log(getTypeOfSchool(16)); // "高校" を表示
 console.log(getTypeOfSchool(30)); // "高校卒業後" を表示
 
 
+line("TDD"); //---------------------------------------------
+
+
+let actual;
+let expected;
+
+function addTen(number) {
+  // ここにコードを書きます
+}
+
+actual = addTen(5);
+expected = 15;
+
+if (actual === expected) {
+  console.log("Yey! Test PASSED.");
+} else {
+  console.log("Test FAILED. Keep trying!");
+  console.log("  actual:", actual);
+  console.log("expected:", expected);
+}
+
+
+
+
+
+
 line("基礎1"); //---------------------------------------------
 
+/**
+ * @param {string} firstName - 下の名前
+ * @param {string} lastName - 苗字
+ * @returns {string} 名前と苗字を連結し、間にスペースを入れたもの
+ */
+function getFullName(firstName, lastName) {
+    return  firstName + " " + lastName;
+  }
+  
+  actual = getFullName("Ken", "Watanabe");
+  expected = "Ken Watanabe";
+  
+  if (actual === expected) {
+    console.log("OK! Test PASSED.");
+  } else {
+    console.error("Test FAILED. Try again!");
+    console.group("Result:");
+    console.log("  actual:", actual);
+    console.log("expected:", expected);
+    console.groupEnd();
+  }
+  
+
+  line("基礎2"); //---------------------------------------------
+
+  /**
+ * @param {string} name - 人の名前
+ * @param {boolean} isFormal - 敬称をつけるべきかどうか（敬称ありなら true、無しなら false）
+ * @returns {string} 与えられた人の名前と敬称が入った挨拶文
+ */
+function simpleGreeting(name, isFormal) {
+    // ここにコードを書きましょう
+    if (isFormal === true) {
+        return "Hello, " + name + "-san." ;
+    } else {
+        return "Hello, " + name + ".";
+    }
+  }
+  
+  actual = simpleGreeting("Amy", true);
+  expected = "Hello, Amy-san.";
+  
+  if (actual === expected) {
+    console.log("OK! Test PASSED.");
+  } else {
+    console.error("Test FAILED. Try again!");
+    console.group("Result:");
+    console.log("  actual:", actual);
+    console.log("expected:", expected);
+    console.groupEnd();
+  }
+  
+  actual = simpleGreeting("Matt", false);
+  expected = "Hello, Matt.";
+  
+  if (actual === expected) {
+    console.log("OK! Test PASSED.");
+  } else {
+    console.error("Test FAILED. Try again!");
+    console.group("Result:");
+    console.log("  actual:", actual);
+    console.log("expected:", expected);
+    console.groupEnd();
+  }
+
+  line("基礎3"); //---------------------------------------------
+
+
+  line("演習"); //---------------------------------------------
+  
+  // function hellomessageforperson(n1){
+  //   if(n1.length>0){
+  //   return `Hello, ${n1}!`;
+  //   }}
+    
+  //   const a = "Beau";
+  //   const aa = hellomessageforperson(a);
+    
+  //   console.log(aa);
+  
+  function sayHello(name){
+    if (name) {
+      return `Hello, ${name}!`;
+    }
+  }
+  
+  const person = "Beau";
+  const message = sayHello(person);
+  
+  console.log(message);
 
 
 
+
+
+
+
+  
