dec5625c0525f9a04bfed11368dfeab93a6dd80a
masato-tsuji <v._.vgentarou@ezweb.ne.jp>
2024-02-04 21:38:45 +0900

    edit

---
+++ presentation/script.js
@@ -0,0 +1,14 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+
+window.onload = function() {
+
+
+    const objElem = document.getElementById("pref23");
+    console.log(objElem);
+
+}
+
+
+
