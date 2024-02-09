3febd51cc1af8cc2b670b85a51aaba2777b367f8
masato-tsuji <v._.vgentarou@ezweb.ne.jp>
2024-02-10 01:48:14 +0900

    update

--- Day11_0207/script.js
+++ Day11_0207/script.js
@@ -149,3 +149,68 @@ function sayFourHellos() {
 sayFourHellos();
 
 
+line("Nightmare 1"); //---------------------------------------------
+//パーティに n 人のゲストが来ています。各ゲストには番号が割り当てられます。
+//各ゲストにすでに到着しているゲストを紹介する関数 getIntroductions を宣言してください。
+/**
+ * @param {number} num - ゲストの数
+ * @returns {Array<string>} 到着するゲストへの挨拶文が入った配列
+ */
+function getIntroductions(num) {
+  const resArray = [];
+  const menbers = [];
+  let string;
+  for (let i = 1; i <= num; i++) {
+    if (i > 1) menbers.push(i - 1);
+    string = menbers.length > 0 ? `${menbers.join(" さん、")} さんです` : "";
+    resArray.push(`${i} さん、ようこそ。${string}`);
+  }
+  return resArray;
+}
+
+test(getIntroductions(1), ["1 さん、ようこそ。"]);
+test(getIntroductions(2), ["1 さん、ようこそ。", "2 さん、ようこそ。1 さんです"]);
+test(getIntroductions(5), [
+  "1 さん、ようこそ。",
+  "2 さん、ようこそ。1 さんです",
+  "3 さん、ようこそ。1 さん、2 さんです",
+  "4 さん、ようこそ。1 さん、2 さん、3 さんです",
+  "5 さん、ようこそ。1 さん、2 さん、3 さん、4 さんです",
+]);
+
+
+line("Nightmare 2"); //---------------------------------------------
+//関数 christmasTree を宣言してください。
+/**
+ * @param {string} string - 任意の 1 文字
+ * @param {number} height - ツリーの高さ
+ * @returns {string} 与えられた文字と高さを使用して、クリスマスツリーを文字列で作る。
+ */
+
+function christmasTree(string, height) {
+  //console.log("#".repeat(5).split("").join(" ").padStart(11));
+  const array = [];
+  for (let i = 0; i < height; i++) {
+    array.push(string.repeat(i + 1).split("").join(" ").padStart(i + height));
+  }
+  console.log(array.join("\n"));
+  return array.join("\n");
+}
+
+const expected1 = "T";
+test(christmasTree("T", 1), expected1);
+const expected2 = " +\n+ +";
+test(christmasTree("+", 2), expected2);
+const expected4 = "   #\n  # #\n # # #\n# # # #";
+test(christmasTree("#", 4), expected4);
+
+// \n は改行を意味するコードです。実際に文字列を console.log すると、ツリーがこんな風に表示されますよ。
+
+/*
+      T
+     T T
+    T T T
+   T T T T
+  T T T T T
+*/
+
