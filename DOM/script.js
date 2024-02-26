e83d571995db913571d3a6678847f845759495df
masato-tsuji <v._.vgentarou@ezweb.ne.jp>
2024-02-26 23:32:57 +0900

    update

--- DOM/script.js
+++ DOM/script.js
@@ -2,48 +2,51 @@
 // 1行目に記載している 'use strict' は削除しないでください
 
 const unicorns = [
-    {
-      name: 'GoldenChocolate',
-      img: 'imgs/01_goldenchocolate.jpg',
-      desc: 'Gentle and kind.'
-    },
-    {
-      name: 'StareingCourage',
-      img: 'imgs/02_stareingcourage.jpg',
-      desc: 'Young unicorn, brother of NobleBreeze.'
-    },
-    {
-      name: 'ObsidianConfidence',
-      img: 'imgs/03_obsidianconfidence.jpg',
-      desc: 'Black knight of Abirian kingdom.'
-    },
-    {
-      name: 'NobleBreeze',
-      img: 'imgs/04_noblebreeze.jpg',
-      desc: 'Princess of Abirian kingdom.'
-    },
-    {
-      name: 'DreamBox',
-      img: 'imgs/05_dreambox.jpg',
-      desc: 'She is full of dreams.'
-    },
-  ];
+  {
+    name: 'GoldenChocolate',
+    img: 'imgs/01_goldenchocolate.jpg',
+    desc: 'Gentle and kind.'
+  },
+  {
+    name: 'StareingCourage',
+    img: 'imgs/02_stareingcourage.jpg',
+    desc: 'Young unicorn, brother of NobleBreeze.'
+  },
+  {
+    name: 'ObsidianConfidence',
+    img: 'imgs/03_obsidianconfidence.jpg',
+    desc: 'Black knight of Abirian kingdom.'
+  },
+  {
+    name: 'NobleBreeze',
+    img: 'imgs/04_noblebreeze.jpg',
+    desc: 'Princess of Abirian kingdom.'
+  },
+  {
+    name: 'DreamBox',
+    img: 'imgs/05_dreambox.jpg',
+    desc: 'She is full of dreams.'
+  },
+];
 
 
 for (const obj of unicorns) {
-    document.body.innerHTML += `<p img=${obj.img} desc="${obj.desc}">${obj.name}</p>`;
-}
-document.body.appendChild(document.createElement("div"));
-
-document.querySelectorAll("p").forEach( elm => {
-    elm.addEventListener("click", (e) => {
-        console.log(e.target.attributes.img.value);
-        document.querySelector("div").innerHTML = 
-            `<img src=${e.target.attributes.img.value}>\
-            <p>${e.target.innerHTML}</p>\
-            <p>${e.target.attributes.desc.value}</P>`;
+  const p = document.createElement("p");
+  p.innerText = obj.name;
+  p.style.cursor = "pointer";
+  document.body.appendChild(p)
+    .addEventListener("click", (e) => {
+      document.querySelector("div").innerHTML = 
+      `<img src=${obj.img}>\
+      <h2>${obj.name}</h2>\
+      <p>${obj.desc}</P>`;
+      document.querySelector("img").style.cssText = "width: 30%; height: 30%;";
     });
-});
+  }
+  
+  document.body.appendChild(document.createElement("div"));
+  
+
 
 
 
