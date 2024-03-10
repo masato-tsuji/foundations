e793a817c1f02a433f1301932a0a51420f89e2e5
masato-tsuji-wsl <gentarou7735@gmail.com>
2024-03-10 15:57:09 +0900

    update

--- Follow_up_assessment/dig-project-random-color/script.js
+++ Follow_up_assessment/dig-project-random-color/script.js
@@ -1,31 +1,20 @@
 'use strict';
 // please do not delete the 'use strict' line above
 
-// ボタン
+// ボタンイベント設定
 const colorBtn = document.getElementById('color-button');
-colorBtn.addEventListener('click', changeColor);
+colorBtn.addEventListener('click', setRandomSliderColor);
 
 
-// スライダー用コンテナ
+// スライダー用コンテナレンダリング
 const divContainer = document.createElement("div");
-divContainer.style.cssText = 
-  "display: grid;\
-   width: 200px;\
-   margin-left: 50px;\
-   padding: 20px;\
-   gap: 12px 0px;\
-   background-color: #FFFFFF;\
-   opacity: 60%;\
-   border: solid 1px #636363;\
-   border-radius: 8px;";
+divContainer.className = "slider-container";
 document.body.appendChild(divContainer);
 
 
-// カラーコード表示
-const elmColorCode = document.createElement("p");
-elmColorCode.style.height = "25px";
-elmColorCode.style.fontSize = "20px";
-elmColorCode.style.fontWeight = "bold";
+// カラーコード表示要素レンダリング
+const elmColorCode = document.createElement("div");
+elmColorCode.className = "color-code-disp";
 divContainer.appendChild(elmColorCode);
 
 
@@ -35,35 +24,35 @@ const makeSlider = (targetElm, colorChangeFunc) => {
     // 色名
     const name = document.createElement("span");
     name.innerText = colorName.toUpperCase();
-    name.style.fontSize = "16px";
+    name.className = "color-name";
     targetElm.appendChild(name);
     // スライダー
-    const elm = document.createElement("input");
-    elm.type = "range";
-    elm.min = 0;
-    elm.max = 255;
-    elm.value = 0;
-    elm.name = colorName;
-    elm.innerText = colorName;
-    elm.disabled = true;
-    targetElm.appendChild(elm);
-    elm.addEventListener("input", changeBodyBgColor);
+    const slider = document.createElement("input");
+    slider.type = "range";
+    slider.min = 0;
+    slider.max = 255;
+    slider.value = 0;
+    slider.name = colorName;
+    slider.innerText = colorName;
+    slider.disabled = true;
+    targetElm.appendChild(slider);
+    slider.addEventListener("input", colorChangeFunc);
     return {
       setVal(val) {
-        elm.value = val;
+        slider.value = val;
       },
       getVal() {
-        return elm.value;
+        return slider.value;
       },
       setDisabled(bool) {
-        elm.disabled = bool;
+        slider.disabled = bool;
       }
     }
   }
   return createElm;
 }
 
-// スライダーレンダリング
+// スライダー要素レンダリング
 const slider = makeSlider(divContainer, changeBodyBgColor);
 const redSlider = slider("red");
 const greenSlider = slider("green");
@@ -71,24 +60,24 @@ const blueSlider = slider("blue");
 const sliders = [redSlider, greenSlider, blueSlider];
 
 
-// 背景色をランダムに変える
-function changeColor() {
-  // ランダム値をスライダーにセット
-  sliders.map( elm => elm.setVal(Math.floor(Math.random() * 255)));
-  // 背景色変更
-  changeBodyBgColor();
-  // スライダー有効化
+// RGBスライダーの各色の値をランダムに生成し背景色変更を呼び出し
+function setRandomSliderColor() {
   sliders.map( elm => elm.setDisabled(false));
+  sliders.map( elm => elm.setVal(Math.floor(Math.random() * 255)));
+  changeBodyBgColor();
 }
 
-// 背景色を各スライドバーで設定されている色に変更
+
+// 背景色をRGBスライドバーで設定されている色に変更
 function changeBodyBgColor() {
   const rgbCode = `RGB(${redSlider.getVal()}, ${greenSlider.getVal()}, ${blueSlider.getVal()})`;
   document.body.style.backgroundColor = rgbCode;
   // RGB値とHEX値を表示
   elmColorCode.innerHTML = 
     rgbCode + "<br>HEX:" +
-    "#" + rgbCode.match(/\d+/g).map( val => {return ("0" + parseInt(val).toString(16)).slice(-2)}).join("");
+    "#" + rgbCode.match(/\d+/g).map( val => {
+       return ("0" + parseInt(val).toString(16)).slice(-2)
+    }).join("");
 }
 
 
