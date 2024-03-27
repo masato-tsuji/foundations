d8fc9eec534a19dada14713625488f7d368f2ba0
masato-tsuji-wsl <gentarou7735@gmail.com>
2024-03-27 23:50:48 +0900

    update

--- Day11_0207/script.js
+++ Day11_0207/script.js
@@ -168,15 +168,37 @@ function getIntroductions(num) {
   return resArray;
 }
 
-test(getIntroductions(1), ["1 さん、ようこそ。"]);
-test(getIntroductions(2), ["1 さん、ようこそ。", "2 さん、ようこそ。1 さんです"]);
-test(getIntroductions(5), [
-  "1 さん、ようこそ。",
-  "2 さん、ようこそ。1 さんです",
-  "3 さん、ようこそ。1 さん、2 さんです",
-  "4 さん、ようこそ。1 さん、2 さん、3 さんです",
-  "5 さん、ようこそ。1 さん、2 さん、3 さん、4 さんです",
-]);
+
+
+function getIntroductions2(num) {
+  const resArray = [];
+  const menbers = [];
+  let string;
+  for (let i = 1; i <= num; i++) {
+    if (i > 1) menbers.push(i - 1);
+    string = menbers.length > 0 ? `${menbers.join(" さん、")} さんです` : "";
+    resArray.push(`${i} さん、ようこそ。${string}`);
+  }
+  return resArray;
+}
+
+
+console.log(getIntroductions2("a"));
+
+
+// test(getIntroductions(1), ["1 さん、ようこそ。"]);
+// test(getIntroductions(2), ["1 さん、ようこそ。", "2 さん、ようこそ。1 さんです"]);
+// test(getIntroductions(5), [
+//   "1 さん、ようこそ。",
+//   "2 さん、ようこそ。1 さんです",
+//   "3 さん、ようこそ。1 さん、2 さんです",
+//   "4 さん、ようこそ。1 さん、2 さん、3 さんです",
+//   "5 さん、ようこそ。1 さん、2 さん、3 さん、4 さんです",
+// ]);
+
+
+
+
 
 
 line("Nightmare 2"); //---------------------------------------------
